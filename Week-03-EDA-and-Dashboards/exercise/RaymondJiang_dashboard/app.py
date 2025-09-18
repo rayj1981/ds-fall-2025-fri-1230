3114271c4c54bffe5973f2bf47145d1c2d9ee5a1
rayj1981 <utctommy904@gmail.com>
2025-09-18 00:56:06 +0000

    finsihed app

--- Week-03-EDA-and-Dashboards/exercise/RaymondJiang_dashboard/app.py
+++ Week-03-EDA-and-Dashboards/exercise/RaymondJiang_dashboard/app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 
 # Load the CSV
 df = pd.read_csv('/workspaces/ds-fall-2025-fri-1230/Week-03-EDA-and-Dashboards/data/movie_ratings.csv')
-
+df.info()
 # Clean the data
 df = df.dropna()
 df = df.drop_duplicates()
@@ -30,8 +30,39 @@ st.bar_chart(genre_counts, horizontal=True)
 
 st.caption("2. Which genres have the highest viewer satisfaction (highest ratings)? ")
 
-genres_by_ratings = df.groupby('genres')['rating']
+genres_by_ratings = df.groupby('genres')['rating'].mean().sort_values(ascending= False)
+
+
+st.bar_chart(genres_by_ratings, use_container_width=True)
+
+
+#Question 3:
+
+st.caption("3.How does mean rating change across movie release years?")
+
+rating_years = df.groupby('year')['rating'].mean().sort_index()
+
+st.bar_chart(rating_years, horizontal=True)
 
 
 
-st.bar_chart(genres_by_ratings, horizontal=True)
+#Question 4:
+
+st.caption("4. What are the 5 best-rated movies that have at least 50 ratings? At least 150 ratings?")
+movie_stats = df.groupby('title').agg(
+    mean_rating=('rating', 'mean'),
+    num_ratings=('rating', 'count')
+)
+
+over_50 = movie_stats[movie_stats['num_ratings'] >= 50]
+
+top5_over_50 = over_50.sort_values('mean_rating', ascending=False).head(5)
+
+st.write("Top 5 movies with at least 50 ratings:")
+st.dataframe(top5_over_50)  
+
+over_150 = movie_stats[movie_stats['num_ratings'] >= 150]
+top5_over_150 = over_150.sort_values('mean_rating', ascending=False).head(5)
+
+st.write("Top 5 movies with at least 150 ratings:")
+st.dataframe(top5_over_150)
